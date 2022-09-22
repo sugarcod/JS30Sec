d1c1472d82a8e5e03b94d1ce8771994d58f1a234
sugarcod <102928648+sugarcod@users.noreply.github.com>
2022-09-22 18:27:48 +0300

    feat: start

---
+++ 02/index.js
@@ -0,0 +1,6 @@
+console.log("io");
+let hour = document.querySelector(".hour-hand");
+let minute = document.querySelector(".min-hand");
+let second = document.querySelector(".second-hand");
+
+let time = new Date();
